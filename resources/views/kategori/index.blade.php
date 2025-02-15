d16dbe0165ef53cf5a598ba0981436240b6c0ad0
fikrinurr12 <fikri030504@gmail.com>
2025-02-15 13:40:26 +0700

    update mematikan asset tidak secure

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -45,9 +45,6 @@
                  'rt' + 
                  '<"row mt-3 d-flex align-items-center"<"col-md-6"i><"col-md-6 text-end"p>>',
             lengthMenu: [[10, 25, 50, 100], [10, 25, 50, 100]], // Opsi pilihan jumlah data
-            
-            dd(route('kategori'));
-
             ajax: "{{ route('kategori') }}",
             columns: [
                 { data: 'nama', name: 'nama' },
